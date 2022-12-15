707b170ac350136792f5624a63394fb93094e999
solar05 <artem00298@gmail.com>
2022-12-15 18:17:30 +0300

    Add vaalidations

--- lib/exisbn.ex
+++ lib/exisbn.ex
@@ -1,26 +1,9 @@
 defmodule Exisbn do
+  require Integer
+
   @moduledoc """
   Documentation for `Exisbn`.
   """
-
-  @doc """
-  Checks if string corresponds isbn10 format (without hyphens!).
-
-  ## Examples
-
-      iex> Exisbn.isbn10?("0545010225")
-      true
-      iex> Exisbn.isbn10?("string")
-      false
-      iex> Exisbn.isbn10?("0545010")
-      false
-
-  """
-  @spec isbn10?(String.t()) :: boolean()
-  def isbn10?(isbn) do
-    length = isbn |> normalize() |> String.length()
-    length == 10
-  end
 
   @doc """
   Takes an ISBN 10 code as string, returns its check digit.
@@ -29,16 +12,11 @@ defmodule Exisbn do
 
     iex> Exisbn.isbn10_checkdigit("85-359-0277")
     5
-    iex> Exisbn.isbn10?("85-359-02775")
-    true
     iex> Exisbn.isbn10_checkdigit("5-02-013850")
     9
-    iex> Exisbn.isbn10?("5-02-0138509")
-    true
     iex> Exisbn.isbn10_checkdigit("0str")
-    {:error, "Incorrect ISBN 10"}
+    nil
   """
-
   def isbn10_checkdigit(isbn) do
     if String.length(normalize(isbn)) in 8..10 do
       nsum =
@@ -55,10 +33,94 @@ defmodule Exisbn do
 
       digit = Integer.mod(11 - Integer.mod(nsum, 11), 11)
 
-      if digit == 10, do: {:error, "Incorrect ISBN 10"}, else: digit
+      if digit == 10, do: "X", else: digit
     else
-      {:error, "Incorrect ISBN 10"}
+      nil
     end
+  end
+
+  @doc """
+  Takes an ISBN 13 code as string, returns its check digit.
+
+  ## Examples
+
+    iex> Exisbn.isbn13_checkdigit("978-5-12345-678")
+    1
+    iex> Exisbn.isbn13_checkdigit("978-0-306-40615")
+    7
+    iex> Exisbn.isbn13_checkdigit("0str")
+    nil
+  """
+  def isbn13_checkdigit(isbn) do
+    if String.length(normalize(isbn)) in 11..13 do
+      nsum =
+        isbn
+        |> normalize()
+        |> String.slice(0..11)
+        |> String.split("", trim: true)
+        |> Enum.map(&to_int/1)
+        |> Enum.with_index()
+        |> Enum.map(fn {val, ind} ->
+          if Integer.is_odd(ind), do: val * 3, else: val
+        end)
+        |> Enum.reduce(&+/2)
+
+      digit = 10 - Integer.mod(nsum, 10)
+
+      if digit == 10, do: 0, else: digit
+    else
+      nil
+    end
+  end
+
+  @doc """
+  Takes an ISBN (10 or 13) and checks its validity by its check digit
+
+  ## Examples
+
+    iex> Exisbn.checkdigit_correct?("85-359-0277-5")
+    true
+    iex> Exisbn.checkdigit_correct?("978-5-12345-678-1")
+    true
+    iex> Exisbn.checkdigit_correct?("978-5-12345-678")
+    false
+  """
+  def checkdigit_correct?(isbn) do
+    normalized = normalize(isbn)
+
+    digit =
+      if String.length(normalized) == 10 do
+        isbn10_checkdigit(normalized)
+      else
+        isbn13_checkdigit(normalized)
+      end
+
+    to_string(digit) == String.last(normalized)
+  end
+
+  @doc """
+  Takes and ISBN (10 or 13) and checks its validity by checking the checkdigit, length and characters.
+
+  ## Examples
+
+    iex> Exisbn.valid?("978-5-12345-678-1")
+    true
+    iex> Exisbn.valid?("978-5-12345-678-1")
+    true
+    iex> Exisbn.valid?("978-5-12345-678")
+    false
+    iex> Exisbn.valid?("85-359-0277-5")
+    true
+    iex> Exisbn.valid?("85-359-0277")
+    false
+  """
+  @spec valid?(String.t()) :: boolean
+  def valid?(isbn) do
+    Enum.all?([
+      correct_length?(isbn),
+      without_incorrect_chars?(isbn),
+      checkdigit_correct?(isbn)
+    ])
   end
 
   defp normalize(isbn) do
@@ -76,6 +138,11 @@ defmodule Exisbn do
     isbn
     |> normalize()
     |> correct_length?()
+  end
+
+  defp isbn10?(isbn) do
+    length = isbn |> normalize() |> String.length()
+    length == 10
   end
 
   defp to_int(char) do
--- test/exisbn_test.exs
+++ test/exisbn_test.exs
@@ -2,7 +2,7 @@ defmodule ExisbnTest do
   use ExUnit.Case
   doctest Exisbn
 
-  test "isbn10? test" do
-    assert Exisbn.isbn10?("0545010225") == true
+  test "isbn10_checkdigit test" do
+    assert Exisbn.isbn10_checkdigit("85-359-0277") == 5
   end
 end
