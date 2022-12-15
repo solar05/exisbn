86b8a78bb56541b5eb1424fe70905023957c0434
solar05 <artem00298@gmail.com>
2022-12-15 04:14:15 +0300

    Add checkdigit isbn10

--- lib/exisbn.ex
+++ lib/exisbn.ex
@@ -22,15 +22,65 @@ defmodule Exisbn do
     length == 10
   end
 
+  @doc """
+  Takes an ISBN 10 code as string, returns its check digit.
+
+  ## Examples
+
+    iex> Exisbn.isbn10_checkdigit("85-359-0277")
+    5
+    iex> Exisbn.isbn10?("85-359-02775")
+    true
+    iex> Exisbn.isbn10_checkdigit("5-02-013850")
+    9
+    iex> Exisbn.isbn10?("5-02-0138509")
+    true
+    iex> Exisbn.isbn10_checkdigit("0str")
+    {:error, "Incorrect ISBN 10"}
+  """
+
+  def isbn10_checkdigit(isbn) do
+    if String.length(normalize(isbn)) in 8..10 do
+      nsum =
+        isbn
+        |> normalize()
+        |> String.slice(0..8)
+        |> String.split("", trim: true)
+        |> Enum.map(&to_int/1)
+        |> Enum.with_index()
+        |> Enum.map(fn {val, ind} ->
+          (10 - ind) * val
+        end)
+        |> Enum.reduce(&+/2)
+
+      digit = Integer.mod(11 - Integer.mod(nsum, 11), 11)
+
+      if digit == 10, do: {:error, "Incorrect ISBN 10"}, else: digit
+    else
+      {:error, "Incorrect ISBN 10"}
+    end
+  end
+
   defp normalize(isbn) do
     isbn
-      |> String.split("", trim: true)
-      |> Enum.filter(fn ch -> is_digit(ch) || ch == "X" end)
-      |> Enum.join()
+    |> String.split("", trim: true)
+    |> Enum.filter(fn ch -> is_digit(ch) || ch == "X" end)
+    |> Enum.join()
   end
 
   defp correct_length?(isbn) do
     Enum.member?([10, 13, 17], String.length(isbn))
+  end
+
+  defp without_incorrect_chars?(isbn) do
+    isbn
+    |> normalize()
+    |> correct_length?()
+  end
+
+  defp to_int(char) do
+    {number, _} = Integer.parse(char)
+    number
   end
 
   defp is_digit(ch) do
