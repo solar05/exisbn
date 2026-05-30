da3e6c638fbadd68e38fa94ef0c3c971d52e3719
solar05 <artem00298@gmail.com>
2026-05-31 02:48:45 +0300

    Release 2.2.4

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Exisbn.MixProject do
   def project do
     [
       app: :exisbn,
-      version: "2.2.3",
+      version: "2.2.4",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       description: description(),
